5ba055c7d86c731083cc14bdf310ddc0bcef1cae
umesh1810 <bobburiumesh18@gmail.com>
2025-02-15 00:15:07 +0530

    1st backend

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,12 +1,37 @@
-import { Pool } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
-import { PrismaClient } from "@prisma/client"
-import { Redis } from "@upstash/redis/cloudflare"
-import { env } from "hono/adapter"
-import { cacheExtension } from "./__internals/db/cache-extension"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
 import { j } from "./__internals/j"
 
+const authMiddleware = j.middleware(async ({ c, next }) => {
+  const authHeader = c.req.header("Authorization")
 
+  if (authHeader) {
+    const apiKey = authHeader.split(" ")[1] // bearer <API_KEY>
+
+    const user = await db.user.findUnique({
+      where: { apiKey },
+    })
+
+    if (user) return next({ user })
+  }
+
+  const auth = await currentUser()
+
+  if (!auth) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
+  return next({ user })
+})
 
 /**
  * Public (unauthenticated) procedures
@@ -14,4 +39,5 @@ import { j } from "./__internals/j"
  * This is the base piece you use to build new queries and mutations on your API.
  */
 export const baseProcedure = j.procedure
-export const publicProcedure = baseProcedure.use(extendedDatabaseMiddleware)
+export const publicProcedure = baseProcedure
+export const privateProcedure = publicProcedure.use(authMiddleware)
